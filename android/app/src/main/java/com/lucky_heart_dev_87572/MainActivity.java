c15c7ec516881d63aef4030edb86da926b4adcc2
Crowdbotics <engineering@crowdbotics.com>
2023-05-31 23:09:38 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/lucky_heart_dev_87572/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lucky_heart_dev_87572;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "lucky_heart_dev_87572";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lucky-heart-dev-87572",
+  url: "https://lucky-heart-dev-87572.botics.co",
+  api: "https://lucky-heart-dev-87572.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
